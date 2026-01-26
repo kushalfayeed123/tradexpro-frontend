9f014e8ec355fcfa78fa0dc34edadc17728591bb
Michael <michael.ajanaku@xprizo.com>
2026-01-26 07:53:26 +0100

    update

--- src/app/investor/layout/sidebar/sidebar.ts
+++ src/app/investor/layout/sidebar/sidebar.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Store } from '@ngxs/store';
+import { Logout } from '../../../auth/state/auth.actions';
+import { NotificationService } from '../../../core/services/notification.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,5 +11,21 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
-onLogout(){}
+    readonly currentYear = new Date().getFullYear();
+  private store = inject(Store);
+  private notify = inject(NotificationService);
+
+
+  
+  async onLogout() {
+    const confirmed = await this.notify.confirm(
+      'Sign Out?',
+      'You will need to enter your credentials to access your account again.',
+      'Sign Out Now'
+    );
+
+    if (confirmed) {
+      this.store.dispatch(new Logout());
+    }
+  }
 }
